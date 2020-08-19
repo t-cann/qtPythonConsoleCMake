55ca0cb7c58f1f0b2dd6f010e38d9e1460d6cfa9
t-cann <tom.cann@live.co.uk>
2020-08-19 11:28:53 +0100

    Initial Commit

---
+++ build/CMakeFiles/embeddingPythonCmake.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/embeddingPythonCmake_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/embeddingPythonCmake_autogen.dir/ParseCache.txt"
+  "embeddingPythonCmake_autogen"
+  "CMakeFiles/embeddingPythonCmake.dir/embeddingPythonCmake_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/embeddingPythonCmake.dir/main.cpp.o"
+  "embeddingPythonCmake"
+  "embeddingPythonCmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/embeddingPythonCmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/embeddingPythonCmake_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/embeddingPythonCmake_autogen"
+  "embeddingPythonCmake_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/embeddingPythonCmake_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
