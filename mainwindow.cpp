101c9bd4f13bba9bd0ece46095b9101b3374ee48
t-cann <tom.cann@live.co.uk>
2020-09-09 09:10:25 +0100

    Improved Central Widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,10 +8,9 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , consoleWidget(new ConsoleWidget())
 {
     
-    QTextEdit* textEdit = new QTextEdit(this);
-    this->setCentralWidget(textEdit);
+    this->createCentralWidget();
     this->createDockWindows();
-    this->setWindowTitle("Test");
+    this->setWindowTitle("Embedded Python Console Test Application");
     this->resize(500,600);    
 }
 
@@ -24,11 +23,41 @@ MainWindow::~MainWindow()
 }
 
 /**
+ * @brief Creates the Central Widget
+ * 
+ */
+void MainWindow::createCentralWidget()
+{
+    QGroupBox* formGroupBox = new QGroupBox(tr("Passing Varibles to Python Console Test:"));
+    QFormLayout *layout = new QFormLayout;
+    
+    QPushButton* button1 = new QPushButton(tr("Int Arrary"));
+    QPushButton* button2 = new QPushButton(tr("Double Arrary"));
+    QPushButton* button3 = new QPushButton(tr("Char Arrary"));
+    
+    layout->addRow(new QLabel(tr("Add Numpy Arrary called x to python console:")), button1);
+    layout->addRow(new QLabel(tr("Add Numpy Arrary called y to python console:")), button2);
+    layout->addRow(new QLabel(tr("TODO Dialog Test")), button3);
+    layout->addRow(new QTextEdit());
+    formGroupBox->setLayout(layout);
+    this->setCentralWidget(formGroupBox);
+
+    //Dummy Data to Test - Sending to Python. 
+    int myNum[3] = {10, 20, 30};
+    double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+    const char* cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    // QObject::connect(button1, &QPushButton::clicked, this, &ConsoleWidget::updateConsole , Qt::QueuedConnection);
+    // QObject::connect(button2, &QPushButton::clicked, this, &ConsoleWidget::updateConsole , Qt::QueuedConnection);
+    // QObject::connect(button3, &QPushButton::clicked, this, &ConsoleWidget::updateConsole , Qt::QueuedConnection);
+}
+
+/**
  * @brief Create all the Dock Windows
  * 
  */
 void MainWindow::createDockWindows(){
-    dock= new QDockWidget(tr("Python Console"), this);
+    QDockWidget* dock = new QDockWidget(tr("Python Console"), this);
     dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea | Qt::BottomDockWidgetArea);
     dock->setWidget(consoleWidget);
     this->addDockWidget(Qt::BottomDockWidgetArea, dock);
